b821ecfd0d2cd519abfd54c0e6a636eb85993a79
Mateo Bernasconi Vargas <bernasconivargas.mateo@gmail.com>
2022-10-04 13:44:30 +0000

    Primera practica

---
+++ fork.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+    printf("Prueba\n");
+    int pid = fork();
+    
+    if (pid == 0) {
+        printf("Soy el proceso hijo \n");
+        sleep(10);
+        printf("ya termine");
+    } else {
+        printf("Soy el proceso padre \n");
+        sleep(10);
+        printf("ya termine padre");
+    }
+    
+    return 0;
+
+}
